01143bf728879e68ae5ad9f51a2d0472c95de72d
Attila Oláh <attilaolah@gmail.com>
2024-02-27 16:09:59 +0100

    Install talhelper via flakes.

--- flake.nix
+++ flake.nix
@@ -3,10 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/23.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+    talhelper.url = "github:budimanjojo/talhelper/v2.3.1";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, talhelper, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -16,6 +17,7 @@
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
+            (talhelper.packages.${system}.default)
             go-task
             kubectl
             talosctl
