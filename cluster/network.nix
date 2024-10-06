17588fdffad0907ed8316ca8184f91908951714b
Attila Oláh <attila@dorn.haus>
2024-10-06 21:24:14 +0200

    Talos: Use DHCP.

--- cluster/network.nix
+++ cluster/network.nix
@@ -11,13 +11,25 @@ in {
     cidr6 = toCIDR net6 net6Len;
   };
   pod = rec {
+    net4 = "10.244.0.0";
+    net4Len = 16;
+    cidr4 = toCIDR net4 net4Len;
+
     net6 = "fd10:244::";
     net6Len = 64;
     cidr6 = toCIDR net6 net6Len;
   };
   service = rec {
+    net4 = "10.96.0.0";
+    net4Len = 12;
+    cidr4 = toCIDR net4 net4Len;
+
     net6 = "fd10:96::";
     net6Len = 108;
     cidr6 = toCIDR net6 net6Len;
+  };
+  uplink = {
+    gw4 = "192.168.1.1";
+    gw6 = "fe80::924d:4aff:fecc:ef8b";
   };
 }
--- talos/talconfig.nix
+++ talos/talconfig.nix
@@ -5,24 +5,14 @@
   hosts = import ../cluster/hosts {inherit pkgs;};
   writeYAML = (pkgs.formats.yaml {}).generate;
 
-  sans = [
-    "::" # api-server
-    "::1" # controller-manager, scheduler
-  ];
+  first = builtins.head hosts.control_plane;
 in
   writeYAML "talconfig.yaml" {
     clusterName = cluster.name;
-    talosVersion = "v1.7.6";
-    kubernetesVersion = "v1.30.2";
-    endpoint = "https://${(builtins.head hosts.control_plane).ipv4}:6443";
+    talosVersion = "v1.8.0";
+    kubernetesVersion = "v1.31.1";
+    endpoint = "https://${first.ipv4}:6443";
     domain = cluster.domain;
-
-    # Disable Flannel CNI.
-    # This is necessary in order to enable Cilium CNI.
-    cniConfig.name = "none";
-
-    additionalMachineCertSans = sans;
-    additionalApiServerCertSans = sans;
 
     # Allow running jobs on control plane nodes.
     # Currently the control plane nodes don't do much anyway.
@@ -31,14 +21,13 @@ in
     nodes =
       map (node: {
         inherit (node) hostname;
-        ipAddress = "${node.ipv4},${node.ipv6}";
+        ipAddress = node.ipv4;
         controlPlane = true;
-        installDisk = "/dev/sda";
+        installDiskSelector.type = "ssd";
         networkInterfaces = [
           {
-            addresses = with node; [net4 net6];
             deviceSelector.hardwareAddr = "*";
-            dhcp = false;
+            dhcp = true;
           }
         ];
       })
@@ -48,35 +37,10 @@ in
       (toYAML {} {
         cluster = {
           network = with cluster.network; {
-            podSubnets = [pod.cidr6];
-            serviceSubnets = [service.cidr6];
+            podSubnets = with pod; [cidr4 cidr6];
+            serviceSubnets = with service; [cidr4 cidr6];
+            # cni = {name = "none";}; # cilium
           };
-
-          # Disable kube-proxy, since we're using
-          # Cilium's kube-proxy replacement functionality.
-          proxy.disabled = true;
-
-          # Prefer IPv6 all interfaces.
-          # TODO: Set up firewall rules to make sure this is not accessible
-          # through the external interfaces (via the IPv6 pinholing setup.)
-          apiServer.extraArgs.bind-address = "::";
-          # Prefer IPv6 loopback for controller-manager and scheduler:
-          controllerManager.extraArgs.bind-address = "::1";
-          scheduler.extraArgs.bind-address = "::1";
-        };
-        machine = {
-          network = {
-            nameservers = [
-              "1.1.1.1" # CloudFlare 1
-              "8.8.8.8" # Google 1
-              "2606:4700:4700::1001" # Cloudflare 2
-              "2001:4860:4860::8844" # Google 2
-            ];
-          };
-          # Prevent the kubelet from using any other address range.
-          # Otherwise the kubelet might pick the auto-configured ::ffff:0:0/96
-          # range, which has no routes configured so requests would fail.
-          kubelet.nodeIP.validSubnets = [cluster.network.node.cidr6];
         };
       })
     ];
